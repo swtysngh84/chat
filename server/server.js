cac59d7a722907a0e482973ca0a5acd76e8e6ae8
swtysngh84 <sweetysingh8469@gmail.com>
2018-12-19 11:14:57 +0530

    connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const path=require('path');
 const express=require('express');
+const http=require('http');
+const socketIO=require('socket.io');
 const publicpath=path.join(__dirname,'../public');
 //console.log(publicpath);
 const port=process.env.PORT || 3000;
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
 app.use(express.static(publicpath));
-app.listen(port,()=>{
+//to register new connection event listener 
+io.on('connection',(socket)=>{
+    console.log('new connection');
+    socket.on('disconnect',()=>{
+        console.log('Disconnected');
+        //user disconnected from server
+        
+    });
+});
+server.listen(port,()=>{
     console.log(`app started at port ${port}`);
 });
